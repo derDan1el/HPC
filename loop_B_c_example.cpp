9dab76e66d2eb10d3c430167a62398bc25534f36
Daniel Schicker <schicker.daniel@gmx.net>
2024-05-18 05:09:57 +0200

    1 seite fertig juhu

---
+++ loop_B_c_example.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    int l_n = 1000000;
+
+
+
+
+
+    for (int l_i = 0; l_i < l_n; l_i++) {
+        Z[l_i] += (X[l_i] + 3.5) * Y[l_i] + 1.5;
+    }
+
+
+    
+    return 0;
+}
